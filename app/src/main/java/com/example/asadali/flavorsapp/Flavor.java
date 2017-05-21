a02fbeace686d0451cbc67f632c51078aac2b60c
asad11111 <asad.ali1010101@gmail.com>
2017-05-22 01:17:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/asadali/flavorsapp/Flavor.java
@@ -0,0 +1,16 @@
+package com.example.asadali.flavorsapp;
+
+/**
+ * Created by Asad Ali on 5/22/2017.
+ */
+public class Flavor {
+    String name;
+    String description;
+    int image;
+
+    public Flavor(String name, String description, int image){
+        this.name = name;
+        this.image = image;
+        this.description = description;
+    }
+}
